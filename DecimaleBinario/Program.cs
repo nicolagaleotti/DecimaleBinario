bfbe2736e35d330a1df5f15bebbe4b77f75ff9cd
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:09:32 +0200

    Add project files.

---
+++ DecimaleBinario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecimaleBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
