4bcf175fbc03c7aec12d49b38845d65fa33e1347
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:40:22 +0200

    inseriti i metodi di conversione binario,ottale e esadecimale

--- LibreriaConversioni/Conversioni.cs
+++ LibreriaConversioni/Conversioni.cs
@@ -4,5 +4,22 @@ namespace LibreriaConversioni
 {
     public class Conversioni
     {
+        public static int Binario(int a)
+        {
+            Console.WriteLine(Convert.ToString(a, 2));
+            return a;
+        }
+        
+        public static int Ottale(int a)
+        {
+            Console.WriteLine(Convert.ToString(a, 8));
+            return a;
+        }
+
+        public static int Esadecimale(int a)
+        {
+            Console.WriteLine(Convert.ToString(a, 16));
+            return a;
+        }
     }
 }
